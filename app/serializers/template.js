6cd200a4cde604a2529c1944d454da0e648da0d3
howardmann <howardmann27@gmail.com>
2017-02-19 22:09:12 +1100

    Refactored nested items

--- app/serializers/template.js
+++ app/serializers/template.js
@@ -5,7 +5,7 @@ export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
     sections: {embedded: 'always'}
   },
   normalizeResponse (store, primaryModelClass, payload, id, requestType){
-    payload = {
+    let newPayload = {
       templates: {
         id: payload.id,
         name: payload.name,
@@ -14,24 +14,33 @@ export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
           .filter(item => item.type === "section")
           .map((section) => {
             section.id = section.item_id;
-            section.items = payload.items
-              .filter((item) => item.parent_id === section.item_id)
-              .map((item) => {
-                item.id = item.item_id;
-                item.subitems = payload.items
-                  .filter(el => el.parent_id === item.item_id)
-                  .map(el => {
-                    el.id = el.item_id;
-                    return el;
-                  })
-                return item;
-              })
             return section;
           })
       }
-    }
+    };
 
+    // Add associated nested items to sections
+    newPayload.templates.sections.map(section => {
+      section.items = payload.items
+        .filter(item => item.parent_id === section.id)
+        .map(item => {
+          item.id = item.item_id;
+          return item;
+        })
+    });
 
-    return this._super(store, primaryModelClass, payload, id, requestType);
+    // Add associated nested subitems to items
+    newPayload.templates.sections.map(section => {
+      section.items.map(sectionItem => {
+        sectionItem.subitems = payload.items
+          .filter(item => item.parent_id === sectionItem.id)
+          .map(item => {
+            item.id = item.item_id;
+            return item;
+          })
+      })
+    })
+
+    return this._super(store, primaryModelClass, newPayload, id, requestType);
   }
 });
